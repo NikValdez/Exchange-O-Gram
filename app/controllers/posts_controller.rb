473a782fc58fe074321afa6876f03ed908086c3d
Nik <nerdvestor@gmail.com>
2017-12-19 11:02:34 -0800

    Style posts and and edit delete actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -14,22 +15,42 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-
-    redirect_to posts_path
+    if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Your new post couldn't be created! Please check the form."
+      render :new
+    end
   end
 
   def destroy
+    if @post.destroy
+      flash[:success] = "Your post has been deleted"
+      redirect_to root_path
+  else
+    flash[:alert] = "Your post could not be deleted"
+    redirect_to posts_path
   end
+end
 
   def update
+    if @post.update(post_params)
+      flash[:success] = "Post updated."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Update failed. Please check the form."
+      render :edit
+    end
   end
 
   private
 
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:image, :caption)
   end
-
 end
